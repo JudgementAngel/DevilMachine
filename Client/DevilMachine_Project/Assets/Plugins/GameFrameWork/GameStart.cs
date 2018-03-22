36286bbb03d2a7c0479d7400b770dea0c36e561c
Move <328419914@qq.com>
2018-03-22 22:03:53 +0800

    DM

--- Client/DevilMachine_Project/Assets/Plugins/GameFrameWork/GameStart.cs
+++ Client/DevilMachine_Project/Assets/Plugins/GameFrameWork/GameStart.cs
@@ -2,8 +2,8 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class GameStart : MonoBehaviour {
-
+public class GameStart : MonoBehaviour
+{
 	// Use this for initialization
 	void Start () {
 		
