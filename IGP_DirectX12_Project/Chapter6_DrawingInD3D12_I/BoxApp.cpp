48932f57ce1d57475c39bf24825b8671bfc52500
328419914@qq.com <328419914@qq.com>
2019-03-25 22:44:07 +0800

    1

--- IGP_DirectX12_Project/Chapter6_DrawingInD3D12_I/BoxApp.cpp
+++ IGP_DirectX12_Project/Chapter6_DrawingInD3D12_I/BoxApp.cpp
@@ -79,4 +79,49 @@ private:
 	POINT mLastMousePos;
 };
 
+int WINAPI WinMain(HINSTANCE hInstance,HINSTANCE prevInstance,PSTR cmdLine,int showCmd)
+{
+	// Enable run-time memory check for debug builds.
+#if defined(DEBUG) | defined(_DEBUG)
+	_CrtSetDbgFlag(_CRTDBG_ALLOC_MEM_DF | _CRTDBG_LEAK_CHECK_DF);
+#endif
+
+	try
+	{
+		BoxApp theApp(hInstance);
+		if (!theApp.Initialize())
+			return 0;
+
+		return theApp.Run();
+	}
+	catch (DxException& e)
+	{
+		MessageBox(nullptr, e.ToString().c_str(), L"HR Failed", MB_OK);
+		return 0;
+	}
+}
+
+BoxApp::BoxApp(HINSTANCE hInstance):D3DApp(hInstance)
+{	
+}
+
+BoxApp::~BoxApp()
+{
+}
+
+bool BoxApp::Initialize()
+{
+	if (!D3DApp::Initialize())
+		return false;
+
+	// Reset the command list to prep for initialization commands.
+	ThrowIfFailed(mCommandList->Reset(mDirectCmdListAlloc.Get(), nullptr));
+
+	BuildDescriptorHeaps();
+	BuildConstBuffers();
+	BuildRootSignature();
+	BuildShadersAndInputLayout();
+}
+
+
  
